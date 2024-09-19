51b48bb58c5cd5e1dd0e6a327163fb0edfb2351b
manikandanmohan10 <manikandan.mohan@softsuave.com>
2024-09-19 16:17:00 +0530

    log

--- app/routes/user.py
+++ app/routes/user.py
@@ -10,20 +10,20 @@ jwt_manager = JWTManager()
 
 @user_router.post("/signup/")
 async def signup(request: Request, user_data: SignUpSchema):
-    logger.info("Signup Request ---> ", user_data)
+    logger.info("Signup Request")
     return await auth_service.signup(user_data)
 
 @user_router.post("/login/")
 async def login(request: Request, user_data: LoginSchema):
-    logger.info("Login Request ---> ", user_data)
+    logger.info("Login Request")
     return await auth_service.login(user_data)
 
 @user_router.post("/account_disable/")
 async def account_disable(request: Request, password: str, current_user: str = Depends(jwt_manager.get_current_user)):
-    logger.info("Account diable Request ---> ", current_user)
+    logger.info("Account diable Request")
     return await auth_service.disable_account(current_user, password)
 
 @user_router.post("/account_enable/")
 async def account_enable(request: Request, user_data: LoginSchema):
-    logger.info("Account enable Request ---> ", user_data)
+    logger.info("Account enable Request")
     return await auth_service.enable_account(user_data)
